e71e8635c7b762cfd995e3aef0d6f0065789d88a
Vidath Hasantha <vidathhasantha96@gmail.com>
2023-06-02 04:46:39 +0530

    Mongodb Setup

--- dbStructure/routes/index.js
+++ dbStructure/routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const PoolClass = require('pg').Pool
+
+const pool = new PoolClass({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'nodepgtest_db',
+    port: 5432,
+    password: '1234'
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  const query = 'SELECT * FROM users'
+    // const dataFromInternet = 1 [dataFromInternet],
+    pool.query(query,(error,dbRes) => {
+        // console.log(dbRes.rows)
+        res.json(dbRes)
+    })
+    
+    pool.end()
 });
 
 module.exports = router;
