bead1f03325129ecbb5bb7f8228618dc3e3e05d5
Vidath Hasantha <vidathhasantha96@gmail.com>
2023-06-02 06:50:06 +0530

    Mongo db uppdate

---
+++ mongoConn/mongoConnect.js
@@ -0,0 +1,17 @@
+const mongodb = require('mongodb')
+// const mongoClient = mongodb.mongoClient
+// const mongoUrl = 'mongodb://localhost:27017/test'
+const { MongoClient } = mongodb;
+const mongoUrl = 'mongodb://localhost:27017';
+
+MongoClient.connect(mongoUrl,(error,databaseConn) => {
+    if(error){
+        console.log('Error occured while connecting to mongo db...!',error)
+        return
+    }
+    const db = databaseConn.db('test')
+    db.collection('New').find({}).toArray((queryError,queryResults) => {
+        console.log(queryResults)
+    })
+    databaseConn.close()
+})
