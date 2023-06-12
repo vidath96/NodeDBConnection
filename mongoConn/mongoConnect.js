5b28dca190cb73b4b679d23f0f4cdb44621f3f74
vidath96 <vidathhasantha96@gmail.com>
2023-06-12 08:58:23 +0530

    Update MongoDbConnect

--- mongoConn/mongoConnect.js
+++ mongoConn/mongoConnect.js
@@ -4,14 +4,21 @@ const mongodb = require('mongodb')
 const { MongoClient } = mongodb;
 const mongoUrl = 'mongodb://localhost:27017';
 
-MongoClient.connect(mongoUrl,(error,databaseConn) => {
+const con = MongoClient.connect(mongoUrl,(error,databaseConn) => {
     if(error){
         console.log('Error occured while connecting to mongo db...!',error)
         return
     }
-    const db = databaseConn.db('test')
-    db.collection('New').find({}).toArray((queryError,queryResults) => {
-        console.log(queryResults)
-    })
-    databaseConn.close()
+    const db = databaseConn.db('student')
+    // db.collection('New').find({}).toArray((queryError,queryResults) => {
+    //     console.log(queryResults)
+    // })
+    // databaseConn.close()
+    return db
 })
+
+module.exports = {
+    query: (queryText,params,callback) => {
+        return con.collection(equeryText,params,callback)
+    }
+}
